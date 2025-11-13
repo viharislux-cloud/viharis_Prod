acb445e231faff342fada4d72c21edf22ac77f0c
viharis.lu <viharislux@gmail.com>
2025-11-13 19:15:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
